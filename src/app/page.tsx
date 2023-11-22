8bf3d6bf871a0ba941ad4ad1a99f58434a802eaa
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-11-22 21:08:30 +0900

    feat: 編集可能コンポーネントを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+"use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Btn from "@/components/user/Btn";
 import Text from "@/components/user/Text";
 import Image from "next/image";
-
+import { Editor, Frame } from "@craftjs/core";
 export default function Home() {
   return (
     <>
-      <Text text="Hello world" />
-      <Btn text="Hello world">ボタン</Btn>
+      <Editor resolver={{ Text, Btn }}>
+        <Frame>
+          <div>
+            <Text text="Hello world" />
+            <Btn text="Hello world">ボタン</Btn>
+          </div>
+        </Frame>
+      </Editor>
     </>
   );
 }
--- src/components/user/Text.tsx
+++ src/components/user/Text.tsx
@@ -1,8 +1,24 @@
+import ContentEditable from "react-contenteditable";
+import { useNode } from "@craftjs/core";
+
 export default function Text({ text }: { text: string }) {
+  const {
+    connectors: { connect, drag },
+    actions: { setProp },
+  } = useNode();
   return (
     <>
       <div>
-        <p>{text}</p>
+        <ContentEditable
+          className="inline-block"
+          html={text}
+          onChange={(e) =>
+            setProp(
+              (props: any) =>
+                (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, ""))
+            )
+          }
+        />
       </div>
     </>
   );
