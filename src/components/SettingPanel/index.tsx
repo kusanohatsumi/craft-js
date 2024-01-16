043fdc7c98e756df7217da48529a3006a03a854e
HatsumiKusano <kusano.hatsumi@gmail.com>
2024-01-16 09:56:11 +0900

    fix:es2015

--- src/components/SettingPanel/index.tsx
+++ src/components/SettingPanel/index.tsx
@@ -1,11 +1,18 @@
 import { useEditor } from "@craftjs/core";
-import { Box, Grid, Typography, Chip, Button as MaterialButton } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Chip,
+  Button as MaterialButton,
+} from "@mui/material";
 import React from "react";
 
 export default function SettingPanel() {
   const { selected } = useEditor((state) => {
     const [currentNodeId] = state.events.selected;
     let selected;
+
     if (currentNodeId) {
       selected = {
         id: currentNodeId,
@@ -15,28 +22,29 @@ export default function SettingPanel() {
           state.nodes[currentNodeId].related.settings,
       };
     }
-    return selected ? (
-        <Box bgcolor="rgba(0, 0, 0, 0.06)" mt={2} px={2} py={2}>
-             <Grid container direction="column" spacing={0}>
+
+    return {
+      selected,
+    };
+  });
+  return selected ? (
+    <Box bgcolor="rgba(0, 0, 0, 0.06)" mt={2} px={2} py={2}>
+      <Grid container direction="column" spacing={0}>
         <Grid item>
           <Box pb={2}>
             <Grid container alignItems="center">
-              <Grid item xs><Typography variant="subtitle1">Selected</Typography></Grid>
-              <Grid item><Chip size="small" color="primary" label={selected.name} /></Grid>
+              <Grid item xs>
+                <Typography variant="subtitle1">Selected</Typography>
+              </Grid>
+              <Grid item>
+                <Chip size="small" color="primary" label={selected.name} />
+              </Grid>
             </Grid>
           </Box>
         </Grid>
-        { 
-          selected.settings && React.createElement(selected.settings)
-        }
-        <MaterialButton
-          variant="contained"
-          
-        >
-          Delete
-        </MaterialButton>
+        {selected.settings && React.createElement(selected.settings)}
+        <MaterialButton variant="contained">Delete</MaterialButton>
       </Grid>
-        </Box>
-    )
-  });
+    </Box>
+  ) : null;
 }
